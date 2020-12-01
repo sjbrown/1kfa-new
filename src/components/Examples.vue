2df00086d108e5945747da99acafb0b1cb8bff57
lunaceee <lunaceee@gmail.com>
2020-11-30 22:49:48 -0800

    add examples page

--- src/components/Examples.vue
+++ src/components/Examples.vue
@@ -1,5 +1,114 @@
 <template>
-  <p>hello</p>
+  <section>
+    <h1 class="text-center m-10">Playtest examples</h1>
+    <h2 class="text-center">Triggering moves</h2>
+    <article class="grid mt-10 justify-items-center">
+      <h3 class="mb-2 text-center">Backstab</h3>
+      <p class="md:mt-4 md:mx-20 m-2">
+        In this clip Kazul (Alex) tries to sneak up behind the lizard priest and
+        perform Backstab. Alex's character hasn't learned the Backstab move yet,
+        but they're allowed to try it at the "wild" level by flipping 3 cards
+        and taking the worst result.
+      </p>
+      <img
+        class="w-screen md:w-64 lg:w-2/5 p-4"
+        src="../assets/images/cards/face35_backstab.png"
+        alt="trigger move backstab"
+      />
+      <div class="mt-8 overflow-hidden relative w-full pb-fluid-video lg:mr-8">
+        <iframe
+          class="absolute top-0 left-0 w-full h-full"
+          src="https://www.youtube.com/embed/3oWjjZ3BsfI"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </article>
+    <article class="grid mt-10 justify-items-center">
+      <h3 class="mb-2 text-center">I know this</h3>
+      <p class="md:mt-4 md:mx-20 m-2">
+        In this clip, the table discusses possible options for the "Yoshi" PC
+        Abigale (Mel), who has lost their sword, and decides they want to get it
+        back, even if they must resort to unconventional means.
+      </p>
+      <img
+        class="w-screen md:w-64 md:float-left lg:w-2/5 p-4"
+        src="../assets/images/cards/face05_i_know_this.png"
+        alt="trigger move I know this"
+      />
+      <div class="mt-8 overflow-hidden relative w-full pb-fluid-video lg:mr-8">
+        <iframe
+          class="absolute top-0 left-0 w-full h-full"
+          src="https://www.youtube.com/embed/USjlUck5fnU"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </article>
+    <article class="grid mt-10 justify-items-center">
+      <h3 class="mb-2 text-center">I know this</h3>
+      <p class="md:mt-4 md:mx-20 m-2">
+        In this clip, Pronce (Michael) triggers Channel The Living Light, to use
+        his magic item, Sling of Unerring Dispatch. After a successful hit, the
+        GM and Michael team up to describe the result.
+      </p>
+      <img
+        class="w-screen md:w-64 md:float-left lg:w-2/5 p-4"
+        src="../assets/images/cards/face05_i_know_this.png"
+        alt="trigger move I know this"
+      />
+      <div class="mt-8 overflow-hidden relative w-full pb-fluid-video lg:mr-8">
+        <iframe
+          class="absolute top-0 left-0 w-full h-full"
+          src="https://www.youtube.com/embed/UKDmL2gF5Iw"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </article>
+    <h2 class="text-center m-20">Examples of play</h2>
+    <article>
+      <div class="p-4 mt-4">
+        <h3 class="mb-2 text-center">Start Script</h3>
+        <p class="grid-flow-col">
+          This is the Start Script (version as of May 10, 2018, when the working
+          title was still "Deckahedron World") that begins a session for players
+          that have never played before.
+        </p>
+      </div>
+      <div class="mt-8 overflow-hidden relative w-full pb-fluid-video">
+        <iframe
+          class="absolute top-0 left-0 w-full h-full"
+          src="https://www.youtube.com/embed/Vvnt06UfOr8"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </article>
+    <article>
+      <div class="p-4 mt-4">
+        <h3 class="mb-2 text-center">Full Session</h3>
+        <p class="grid-flow-col">
+          A full session of play (version as of May 10, 2018, when the working
+          title was still "Deckahedron World") where the players have never
+          played a role-playing game before.
+        </p>
+      </div>
+      <div class="mt-8 overflow-hidden relative w-full pb-fluid-video">
+        <iframe
+          class="absolute top-0 left-0 w-full h-full"
+          src="https://www.youtube.com/embed/K2uRJQa4F8o"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </article>
+  </section>
 </template>
 <script>
 export default {
