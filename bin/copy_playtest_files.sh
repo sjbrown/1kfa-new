04722c48b7136cd1fb4c56dba696491c602d11f8
Shandy Brown <github@ezide.com>
2021-01-17 18:31:51 -0800

    update scripts

---
+++ bin/copy_playtest_files.sh
@@ -0,0 +1,72 @@
+#! /bin/bash
+
+set -e
+if [ $DEBUG ]; then
+  set -x
+fi
+
+if [ `basename $(pwd)` != "build" ]; then
+  echo "You must be in the build directory to run this command"
+  exit 1
+fi
+
+echo "----"
+echo "Beginning copy_playtest_files.sh"
+
+BUILDDIR=$PWD
+
+echo "-----------------------------------------------------"
+echo ""
+echo " Copying deckahedron_site repo"
+echo ""
+echo "-----------------------------------------------------"
+echo ""
+
+if [ -e deckahedron_site/.git ]
+then
+  cd deckahedron_site
+  git status
+  git fetch origin
+  git rebase origin/master
+  cd $BUILDDIR
+else
+  rm -rf deckahedron_site
+  git clone https://github.com/sjbrown/deckahedron_site.git
+fi
+
+
+echo "-----------------------------------------------------"
+echo ""
+echo " Copying 1kfa repo"
+echo ""
+echo "-----------------------------------------------------"
+echo ""
+
+
+if [ -e 1kfa/.git ]
+then
+  cd 1kfa
+  git status
+  git fetch origin
+  git rebase origin/master
+  cd $BUILDDIR
+else
+  rm -rf 1kfa
+  git clone https://github.com/sjbrown/1kfa.git
+fi
+
+source 1kfa/resolution_cards/version.py # Get the VERSION variable
+export KFAREPO=$BUILDDIR/1kfa
+
+echo "-----------------------------------------------------"
+echo ""
+echo " Copying 1kFA version $VERSION playtest_files"
+echo ""
+echo "-----------------------------------------------------"
+echo ""
+
+cp deckahedron_site/dist/$VERSION/1kfa_playtest.tar.gz ./
+tar -xvzf 1kfa_playtest.tar.gz
+cp 1kfa_playtest/1kfa_guide_*.* playtest_files/
+cp 1kfa_playtest.tar.gz playtest_files/1kfa_playtest.tgz
+
---
+++ bin/copy_table.sh
@@ -0,0 +1,52 @@
+#! /bin/bash
+
+set -e
+
+if [ `basename $(pwd)` != "build" ]; then
+  echo "You must be in the build directory to run this command"
+  exit 1
+fi
+
+DESTDIR=table
+echo "-----------------------------------------------------"
+echo ""
+echo " Copying togetherness to ./$DESTDIR"
+echo ""
+echo "-----------------------------------------------------"
+echo ""
+
+if [ -e togetherness/.git ]
+then
+  cd togetherness
+  git status
+  git fetch origin master
+  git fetch origin beta
+  git rebase origin/master
+  cd ..
+else
+  rm -rf togetherness
+  git clone https://github.com/sjbrown/togetherness.git
+fi
+
+rm -rf $DESTDIR
+cp -a togetherness/src $DESTDIR
+
+
+DESTDIR=table_beta
+echo "-----------------------------------------------------"
+echo ""
+echo " Copying togetherness (beta branch) to ./$DESTDIR"
+echo ""
+echo "-----------------------------------------------------"
+echo ""
+cd togetherness
+git checkout beta
+cd ..
+rm -rf $DESTDIR
+cp -a togetherness/src $DESTDIR
+
+
+echo "Finished!"
+echo "---------"
+
+
