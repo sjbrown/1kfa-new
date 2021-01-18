fbaff07e247b2984f22c9311c016013f1c5fa00b
Shandy Brown <github@ezide.com>
2021-01-17 18:31:51 -0800

    update scripts

---
+++ bin/copy_cards.sh
@@ -0,0 +1,63 @@
+#! /bin/bash
+
+set -e
+if [ $DEBUG ]; then
+  set -x
+fi
+
+if [ `basename $(pwd)` != "build" ]; then
+  echo "You must be in the build directory to run this command"
+  exit 1
+fi
+
+echo "----"
+echo "Beginning copy_cards.sh"
+
+BUILDDIR=$PWD
+
+echo "-----------------------------------------------------"
+echo ""
+echo " Copying 1kFA version $VERSION assets/cards..."
+echo ""
+echo "-----------------------------------------------------"
+echo ""
+
+rm -rf "assets/cards_v$VERSION"
+cp deckahedron_site/dist/$VERSION/cards_v$VERSION.tar.gz ./
+tar -xvzf cards_v$VERSION.tar.gz
+cp -a cards_v$VERSION ./assets/cards_v$VERSION
+
+cd "assets/cards_v$VERSION"
+echo '<html><head><style>' > index.html
+echo '</style></head><body>' >> index.html
+echo '<h1><a href="booklet">Booklet pages</a></h1>' >> index.html
+echo '<h1><a href="moves">Move cards</a></h1>' >> index.html
+echo '<h1><a href="items">Item cards</a></h1>' >> index.html
+
+
+mkdir -p booklet
+echo '<html><head><style>' > booklet/index.html
+echo 'a { margin: 20px; display: inline-block; max-width: 128px; overflow-wrap: break-word; }' >> booklet/index.html
+echo 'img { max-width: 128px; }' >> booklet/index.html
+echo '</style></head><body>' >> booklet/index.html
+find . |grep booklet |grep png |sort |awk '{ print "<a href=\"../" $1 "\"><img src=\"../" $1 "\"> " $1 "</a>" }' >> booklet/index.html
+
+mkdir -p items
+echo '<html><head><style>' > items/index.html
+echo 'a { margin: 20px; display: inline-block; max-width: 128px; overflow-wrap: break-word; }' >> items/index.html
+echo 'img { max-width: 128px; }' >> items/index.html
+echo '</style></head><body>' >> items/index.html
+find . |grep mundane_deck |grep png |sort |awk '{ print "<a href=\"../" $1 "\"><img src=\"../" $1 "\"> " $1 "</a>" }' >> items/index.html
+find . |grep magic_deck |grep png |sort |awk '{ print "<a href=\"../" $1 "\"><img src=\"../" $1 "\"> " $1 "</a>" }' >> items/index.html
+
+mkdir -p moves
+echo '<html><head><style>' > moves/index.html
+echo 'a { margin: 20px; display: inline-block; max-width: 128px; overflow-wrap: break-word; }' >> moves/index.html
+echo 'img { max-width: 128px; }' >> moves/index.html
+echo '</style></head><body>' >> moves/index.html
+find . |grep starter |grep png |sort |awk '{ print "<a href=\"../" $1 "\"><img src=\"../" $1 "\"> " $1 "</a>" }' >> moves/index.html
+find . |grep move_deck |grep png |sort |awk '{ print "<a href=\"../" $1 "\"><img src=\"../" $1 "\"> " $1 "</a>" }' >> moves/index.html
+
+cd $BUILDDIR
+
+
--- bin/netlify_build.sh
+++ bin/netlify_build.sh
@@ -1,4 +1,41 @@
 #! /bin/bash
 
-echo "hello"
-echo $PWD
+if [ $DEBUG ]; then
+  set -x
+fi
+
+echo "----"
+echo "Beginning 1kFA Netlify Build Script"
+
+ORIGDIR=$PWD
+echo "my pwd is $PWD"
+echo "my home is $HOME"
+export PATH=$PATH:$PWD/bin
+
+SCRATCHDIR=$PWD/build
+
+if [ $FORCE_REBUILD -eq 1 ]; then
+  rm -rf $SCRATCHDIR
+fi
+mkdir -p $SCRATCHDIR
+
+cd $SCRATCHDIR
+copy_playtest_files.sh
+copy_cards.sh
+copy_table.sh
+cd $ORIGDIR
+
+cp -a $SCRATCHDIR/playtest_files ./dist/playtest_files
+cp -a $SCRATCHDIR/assets ./dist/assets
+cp -a $SCRATCHDIR/table ./dist/table
+cp -a $SCRATCHDIR/table_beta ./dist/table_beta
+
+# Scrape git to populate the LATEST_UPDATE section on the home page
+cd $SCRATCHDIR/1kfa
+UPDATE=`git log |grep '\[UPDATE\]' | head -1 - | awk '{$1=""; print $0}'`
+UPDATE_SPACE_TRIMMED="$(sed -e 's/[[:space:]]*$//' <<<${UPDATE})"
+echo "Last update: $UPDATE_SPACE_TRIMMED"
+
+
+echo "Finished! 1kFA Netlify Build Script"
+echo "----"
