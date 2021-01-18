81cd084967e0517362af2f5d87711b0598442c15
Shandy Brown <github@ezide.com>
2021-01-17 21:45:15 -0800

    Beta (#1)
    * add debugging for bin/copy_table.sh
    * fix cloudflare beacon

--- bin/copy_table.sh
+++ bin/copy_table.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
 set -e
+if [ $DEBUG ]; then
+  set -x
+fi
 
 if [ `basename $(pwd)` != "build" ]; then
   echo "You must be in the build directory to run this command"
@@ -18,7 +21,6 @@ echo ""
 if [ -e togetherness/.git ]
 then
   cd togetherness
-  git status
   git fetch origin master
   git fetch origin beta
   git rebase origin/master
@@ -30,6 +32,15 @@ fi
 
 rm -rf $DESTDIR
 cp -a togetherness/src $DESTDIR
+
+BEACON="'{\"token\": \"ad4bc0439a524824ac7ccf972f3a286b\"}'"
+
+sed -i '/<\/head>/i<!-- Cloudflare -->' $DESTDIR/index.html
+sed -i '/<\/head>/i<script defer data-domain="1kfa.com"' $DESTDIR/index.html
+sed -i '/<\/head>/i src="https:\/\/static.cloudflareinsights.com\/beacon.min.js"' $DESTDIR/index.html
+sed -i "/<\/head>/i data-cf-beacon=$BEACON" $DESTDIR/index.html
+sed -i '/<\/head>/i ><\/script>' $DESTDIR/index.html
+sed -i '/<\/head>/i<!-- END Cloudflare -->' $DESTDIR/index.html
 
 
 DESTDIR=table_beta
@@ -44,6 +55,14 @@ git checkout beta
 cd ..
 rm -rf $DESTDIR
 cp -a togetherness/src $DESTDIR
+
+sed -i '/<\/head>/i<!-- Cloudflare -->' $DESTDIR/index.html
+sed -i '/<\/head>/i<script defer data-domain="1kfa.com"' $DESTDIR/index.html
+sed -i '/<\/head>/i src="https:\/\/static.cloudflareinsights.com\/beacon.min.js"' $DESTDIR/index.html
+sed -i "/<\/head>/i data-cf-beacon=$BEACON" $DESTDIR/index.html
+sed -i '/<\/head>/i ><\/script>' $DESTDIR/index.html
+sed -i '/<\/head>/i<!-- END Cloudflare -->' $DESTDIR/index.html
+
 
 
 echo "Finished!"
