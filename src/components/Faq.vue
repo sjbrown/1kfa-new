4722b50b25ac197fcc803fdf31bb707efb73d933
Shandy Brown <github@ezide.com>
2021-01-18 22:37:25 -0800

    Beta (#3)
    * marketing: meta tags and favicon
    add facebook meta tags
    add twitter meta tags
    add marketing_cover.jpg
    * add content to the FAQ
    * margins
    * alingment
    * redo kim page art

--- src/components/Faq.vue
+++ src/components/Faq.vue
@@ -4,14 +4,14 @@
       <img
         class="object-contain"
         src="../assets/images/cover-art/cover_guillermo_1200x1600.jpg"
-        alt="cover art from guillermo"
+        alt="page art from guillermo"
       />
     </div>
     <ul
       class="grid max-w-2xl m-auto p-8 -mt-80 lg:mt-40 lg:-ml-20 bg-gray-50 border-t-4 border-yellow-400"
     >
       <li>
-        <h2 class="mb-8">What's Role-playing?</h2>
+        <h2 class="my-8">What's Role-playing?</h2>
         <p>
           Like a board game, a role-playing game is played by sitting around a
           table with your friends. <br />But, as a role-playing game, A Thousand
@@ -26,12 +26,93 @@
           series of milestones and prompts, but you need to play to find out the
           details of what happens
         </p>
+        <h2 class="my-8">Is this just like Dungeons &amp; Dragons?</h2>
+        <p>
+          Well... <i>a little bit</i>.
+          <br />
+          Like D&amp;D (and many other role-playing games), the story of the
+          game takes place in a shared, imagined universe limited only by
+          the imaginations of the players.
+          There will also be fantasy combat involving swords and magic.
+          <br />
+          A Thousand Faces of Adventure is built for creating stories that
+          have, at their core, a structure called "The Hero's Journey".
+          Specific rules cause the characters in the story to
+          cross a threshold into a mysterious, unknown world, where they find
+          a special prize, and then they return to their home as agents of
+          meaningful change.
+          <br />
+          There are no "adventure modules" or pre-set story though - the
+          challenge for the GM is the challenge of playing jazz: improvising
+          and letting wondrous story details emerge naturally, all the
+          while earning points by keeping to the structure.
+          <br />
+          Another difference is the physical format. A Thousand Faces of
+          Adventure is played with tokens and cards, with all the necessary
+          information right there on the tabletop, and not encoded on pages
+          in a thick book.
+          <br />
+          For the players, the challenge is not miniatures-based grid combat,
+          but rather deck-building and managing the economy of XP tokens and
+          mitigating risk by smart uses of "move" cards.
+        </p>
+        <h2 class="my-8">How long does it take to play?</h2>
+        <p>
+          There are 3 formats included, a 3-hour (aka "One Shot"), a 9-hour,
+          and a 30-hour campaign.
+        </p>
+        <h2 class="my-8">How much does it cost?</h2>
+        <p>
+          The retail cost has yet to be determined. Today, the "playtest"
+          version is available for free. Follow @boardcrafting on Twitter
+          or join discussions on the <code>/r/RPGdesign</code> subreddit
+          to get updates about the availability of a retail product.
+        </p>
+        <h2 class="my-8">How many players are needed?</h2>
+        <p>
+          The game plays best with between 3 and 5 players. One of those
+          players will be the GM, and the rest will play the adventurers
+          of the story.
+        </p>
+        <h2 class="my-8">What do I need to play?</h2>
+        <p>
+          The playtest kit available on the Downloads page includes
+          everything you will need to print-and-play at home. Additionally,
+          there is a digital version of the Deckahedron (a deck of 20 cards
+          players need to generate random results) at 
+          <a href="https://www.1kfa.com/table">Togetherness Table</a>
+          <br />
+          (This tool has many other uses, the Deckahedron is found by clicking
+          the "Cards" button)
+        </p>
+        <h2 class="my-8">Is there an online version?</h2>
+        <p>
+          A Thousand Faces of Adventure is 100% designed to be played with
+          other human beings, ideally in person, but failing that, it can be
+          played with video-conferencing tools and the above-mentioned
+          <a href="https://www.1kfa.com/table">Togetherness Table</a>.
+        </p>
+        <h2 class="my-8">Is the GM playing *against* the other players?</h2>
+        <p>
+          The GM should
+          <a href="https://rpg.stackexchange.com/questions/161727/what-does-be-a-fan-of-the-characters-mean">be a fan of the characters</a>.
+          But the GM is not a "god" of the shared, imagined universe of the
+          game. The GM cannot just "do anything". In A Thousand Faces of
+          Adventure, the GM is another player following rules, but their
+          rules are different from the others.
+          <br />
+          During play, GMs collect points, "Journey Points" and "Shadow Points".
+          With Shadow Points, a GM can increase the difficulty of the challenges
+          the other players face. With Journey Points, the GM can "break" or
+          "bend" certain other rules of the game, even perform "miracles", like
+          bringing dead characters back to life.
+        </p>
       </li>
     </ul>
-    <div class="w-full lg:mt-56">
+    <div class="w-full lg:mt-56 self-end">
       <img
         src="../assets/images/cover-art/cover_kim_1200x1600.jpg"
-        alt="cover art brandon"
+        alt="page art kim"
       />
     </div>
   </section>
