ec03d94a2afa9adff2117ba924d750e71ac88a1e
Shandy Brown <github@ezide.com>
2025-06-23 20:09:29 -0700

    fix build scripts

--- bin/netlify_build.sh
+++ bin/netlify_build.sh
@@ -29,6 +29,9 @@ cp -a $SCRATCHDIR/playtest_files ./dist/playtest_files
 cp -a $SCRATCHDIR/assets ./dist/assets
 cp -a $SCRATCHDIR/table ./dist/table
 cp -a $SCRATCHDIR/table/deckahedron ./dist/deckahedron
+cp -a $SCRATCHDIR/table/img ./dist/img
+cp -a $SCRATCHDIR/table/svg ./dist/svg
+cp -a $SCRATCHDIR/table/animate.css ./dist/animate.css
 cp -a $SCRATCHDIR/table_beta ./dist/table_beta
 
 # Scrape git to populate the LATEST_UPDATE section on the home page
