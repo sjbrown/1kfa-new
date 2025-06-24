49c632052dd23939b285055933dcc33f01659bcc
Shandy Brown <github@ezide.com>
2025-06-23 18:31:49 -0700

    try to fix deploy script

--- bin/copy_table.sh
+++ bin/copy_table.sh
@@ -10,6 +10,8 @@ if [ `basename $(pwd)` != "build" ]; then
   exit 1
 fi
 
+BEACON="'{\"token\": \"ad4bc0439a524824ac7ccf972f3a286b\"}'"
+
 DESTDIR=table
 echo "-----------------------------------------------------"
 echo ""
@@ -22,7 +24,24 @@ git clone --depth=1 https://github.com/sjbrown/togetherness.git
 rm -rf $DESTDIR
 cp -a togetherness/src $DESTDIR
 
-BEACON="'{\"token\": \"ad4bc0439a524824ac7ccf972f3a286b\"}'"
+
+sed -i '/<\/head>/i<!-- Cloudflare -->' $DESTDIR/index.html
+sed -i '/<\/head>/i<script defer data-domain="1kfa.com"' $DESTDIR/index.html
+sed -i '/<\/head>/i src="https:\/\/static.cloudflareinsights.com\/beacon.min.js"' $DESTDIR/index.html
+sed -i "/<\/head>/i data-cf-beacon=$BEACON" $DESTDIR/index.html
+sed -i '/<\/head>/i ><\/script>' $DESTDIR/index.html
+sed -i '/<\/head>/i<!-- END Cloudflare -->' $DESTDIR/index.html
+
+
+DESTDIR=deckahedron
+echo "-----------------------------------------------------"
+echo ""
+echo " Copying togetherness/src/deckahedron to ./$DESTDIR"
+echo ""
+echo "-----------------------------------------------------"
+echo ""
+rm -rf $DESTDIR
+cp -a togetherness/src/deckahedron $DESTDIR
 
 sed -i '/<\/head>/i<!-- Cloudflare -->' $DESTDIR/index.html
 sed -i '/<\/head>/i<script defer data-domain="1kfa.com"' $DESTDIR/index.html
@@ -43,23 +62,6 @@ rm -rf t_beta
 git clone --depth=1 --branch=beta https://github.com/sjbrown/togetherness.git t_beta
 rm -rf $DESTDIR
 cp -a t_beta/src $DESTDIR
-
-sed -i '/<\/head>/i<!-- Cloudflare -->' $DESTDIR/index.html
-sed -i '/<\/head>/i<script defer data-domain="1kfa.com"' $DESTDIR/index.html
-sed -i '/<\/head>/i src="https:\/\/static.cloudflareinsights.com\/beacon.min.js"' $DESTDIR/index.html
-sed -i "/<\/head>/i data-cf-beacon=$BEACON" $DESTDIR/index.html
-sed -i '/<\/head>/i ><\/script>' $DESTDIR/index.html
-sed -i '/<\/head>/i<!-- END Cloudflare -->' $DESTDIR/index.html
-
-DESTDIR=deckahedron
-echo "-----------------------------------------------------"
-echo ""
-echo " Copying togetherness/src/deckahedron to ./$DESTDIR"
-echo ""
-echo "-----------------------------------------------------"
-echo ""
-rm -rf $DESTDIR
-cp -a togetherness/src/deckahedron $DESTDIR
 
 sed -i '/<\/head>/i<!-- Cloudflare -->' $DESTDIR/index.html
 sed -i '/<\/head>/i<script defer data-domain="1kfa.com"' $DESTDIR/index.html
